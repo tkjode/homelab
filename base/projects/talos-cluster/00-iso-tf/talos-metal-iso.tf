3026161250a0e6df505c052464b2fdc9387d8671
TKJode <tkjode@gmail.com>
2025-04-10 18:15:30 -0400

    feat(talos-iso): fix missing variable declarations

--- base/projects/talos-cluster/00-iso-tf/talos-metal-iso.tf
+++ base/projects/talos-cluster/00-iso-tf/talos-metal-iso.tf
@@ -1,7 +1,19 @@
+variable "iso_datastore" {
+  type = "string"
+}
+
+variable "talos_iso_version" {
+  type = "string"
+}
+
+variable "proxmox_node" {
+  type = "string"
+}
+
 resource "proxmox_virtual_environment_download_file" "talos-metal-iso" {
   content_type  = "iso"
   datastore_id  = var.iso_datastore
   file_name     = "talos-metal-${ var.talos_iso_version }.iso"
-  node_name     = var.PROXMOX_TARGET
+  node_name     = var.proxmox_node
   url           = "https://github.com/siderolabs/talos/releases/download/${ var.talos_iso_version }/metal-amd64.iso"
 }
