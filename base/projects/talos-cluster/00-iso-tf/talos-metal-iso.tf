e10e2a9991cae76dba65acc858484d8ba412f7b6
TKJode <tkjode@gmail.com>
2025-04-10 18:01:23 -0400

    fix(talos-iso): providing s3 backend creds via repo secrets

--- base/projects/talos-cluster/00-iso-tf/talos-metal-iso.tf
+++ base/projects/talos-cluster/00-iso-tf/talos-metal-iso.tf
@@ -1,7 +1,7 @@
 resource "proxmox_virtual_environment_download_file" "talos-metal-iso" {
   content_type  = "iso"
-  datastore_id  = ${var.iso_datastore}
-  file_name     = "talos-metal-${var.talos_iso_version}.iso"
-  node_name     = ${var.PROXMOX_TARGET}
-  url           = "https://github.com/siderolabs/talos/releases/download/${var.talos_iso_version}/metal-amd64.iso"
+  datastore_id  = var.iso_datastore
+  file_name     = "talos-metal-${ var.talos_iso_version }.iso"
+  node_name     = var.PROXMOX_TARGET
+  url           = "https://github.com/siderolabs/talos/releases/download/${ var.talos_iso_version }/metal-amd64.iso"
 }
