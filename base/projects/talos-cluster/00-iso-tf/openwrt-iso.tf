10dc53c73925a6768c40caa129658e45f8971cee
TKJode <tkjode@gmail.com>
2025-04-12 13:36:55 -0400

    fix(talos-iso): Removing gz compression directive as it's attempting to double-decompress

--- base/projects/talos-cluster/00-iso-tf/openwrt-iso.tf
+++ base/projects/talos-cluster/00-iso-tf/openwrt-iso.tf
@@ -1,6 +1,6 @@
 resource "proxmox_virtual_environment_download_file" "openwrt-image-gz" {
   content_type            = "iso"
-  decompression_algorithm = "gz"
+  #decompression_algorithm = "gz"
   overwrite_unmanaged     = "true"
   datastore_id            = var.image_datastore
   file_name               = "openwrt-${ var.openwrt_release }-x86_64.img"
