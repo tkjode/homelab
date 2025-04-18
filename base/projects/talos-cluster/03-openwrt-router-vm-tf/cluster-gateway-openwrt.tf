deed943ec4a7411afe85c33ac448c0be0ca01762
TKJode <tkjode@gmail.com>
2025-04-18 18:15:26 -0400

    feat(talos): start to deploy OpenWRT VM using remote backend config data

---
+++ base/projects/talos-cluster/03-openwrt-router-vm-tf/cluster-gateway-openwrt.tf
@@ -0,0 +1,56 @@
+data "terraform_remote_state" "iso" {
+  backend = "s3"
+  config = {
+    bucket  = "phalnet-homelab-automation"
+    key     = "talos/iso"
+    region  = "ca-central-1"
+  }
+}
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+  config = {
+    bucket    = "phalnet-homelab-automation"
+    key       = "talos/network"
+    region    = "ca-central-1"
+  }
+}
+
+data "terraform_remote_state" "pool" {
+  backend = "s3"
+  config = {
+    bucket    = "phalnet-homelab-automation"
+    key       = "talos/pool"
+    region    = "ca-central-1"
+  }
+}
+
+
+resource "proxmox_virtual_environment_vm" "gateway" {
+  name      = "gateway-openwrt"
+  description = "OpenWRT router gateway through bridge networks and DHCP Provider"
+  tags        = ["talos", "openwrt"]
+
+  cpu {
+    cores   = 2
+    type    = "x86-64-v2-AES"
+  }
+
+  memory {
+    dedicated   = 2048
+    floating    = 2048
+  }
+
+  disk  {
+    datastore_id  = "SSD"
+    file_id       = data.terraform_remote_state.iso.openwrt-disk-image
+    interface     = "virtio0"
+  }
+
+  network_device {
+      bridge  = data.terraform_remote_state.network.cluster_bridge_id
+  }
+
+
+
+}
