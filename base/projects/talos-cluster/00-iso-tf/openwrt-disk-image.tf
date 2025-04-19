0ce5544d7a727e8bd0ed68a4c46603362b267b94
TKJode <tkjode@gmail.com>
2025-04-19 12:16:34 -0400

    chore(talos): renaming openwrt image as its not an ISO per se and testing squashfs image for bootability

--- base/projects/talos-cluster/00-iso-tf/openwrt-disk-image.tf
+++ base/projects/talos-cluster/00-iso-tf/openwrt-disk-image.tf
@@ -5,7 +5,7 @@ resource "proxmox_virtual_environment_download_file" "openwrt-image-gz" {
   datastore_id            = var.image_datastore
   file_name               = "openwrt-${ var.openwrt_release }-x86_64.img"
   node_name               = var.proxmox_node
-  url                     = "https://downloads.openwrt.org/releases/${var.openwrt_release}/targets/x86/64/openwrt-${ var.openwrt_release }-x86-64-generic-ext4-combined.img.gz"
+  url                     = "https://downloads.openwrt.org/releases/${var.openwrt_release}/targets/x86/64/openwrt-${ var.openwrt_release }-x86-64-generic-squashfs-combined.img.gz"
 }
 
 output "openwrt-disk-image" {
