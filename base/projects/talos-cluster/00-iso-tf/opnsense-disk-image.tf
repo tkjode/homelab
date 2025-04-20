3922ab8b675ce39169913b07fc8dd666b298efa0
TKJode <tkjode@gmail.com>
2025-04-19 21:51:08 -0400

    refactor(talos): trying the dvd image

--- base/projects/talos-cluster/00-iso-tf/opnsense-disk-image.tf
+++ base/projects/talos-cluster/00-iso-tf/opnsense-disk-image.tf
@@ -3,9 +3,9 @@ resource "proxmox_virtual_environment_download_file" "opnsense-disk-image" {
   #decompression_algorithm = "gz"
   overwrite_unmanaged     = "true"
   datastore_id            = var.image_datastore
-  file_name               = "opnsense-25.1-vga-amd64.img"
+  file_name               = "opnsense-25.1-dvd-amd64.iso"
   node_name               = var.proxmox_node
-  url                     = "https://mirror.ams1.nl.leaseweb.net/opnsense/releases/25.1/OPNsense-25.1-vga-amd64.img.bz2"
+  url                     = "https://mirror.ams1.nl.leaseweb.net/opnsense/releases/25.1/OPNsense-25.1-dvd-amd64.iso.bz2"
 }
 
 output "opnsense-disk-image" {
