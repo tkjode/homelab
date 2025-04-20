187f9f8f41e8721d447a1d1a5462f8aabf5ddaea
TKJode <tkjode@gmail.com>
2025-04-19 22:03:27 -0400

    fix(talos): affix iso to filename, expect bzcat errors next

--- base/projects/talos-cluster/00-iso-tf/opnsense-disk-image.tf
+++ base/projects/talos-cluster/00-iso-tf/opnsense-disk-image.tf
@@ -3,7 +3,7 @@ resource "proxmox_virtual_environment_download_file" "opnsense-disk-image" {
   decompression_algorithm = "bz2"
   overwrite_unmanaged     = "true"
   datastore_id            = var.image_datastore
-  #file_name               = "opnsense-25.1-dvd-amd64.iso"
+  file_name               = "opnsense-25.1-dvd-amd64.bz2.iso"
   node_name               = var.proxmox_node
   url                     = "https://mirror.ams1.nl.leaseweb.net/opnsense/releases/25.1/OPNsense-25.1-dvd-amd64.iso.bz2"
 }
