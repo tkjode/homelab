8599dccbfc618f9c3406c8c5b952e70fa64a49b7
TKJode <tkjode@gmail.com>
2025-10-14 08:41:50 -0400

    chore(regulus): templating cloud-inits for vms

---
+++ base/builds/regulus/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.85.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "phalnet-homelab-automation"
+    key = "regulus/infrastructure"
+    region = "ca-central-1"
+
+  }
+
+}
+
+variable "proxmox_node" {
+  type = string
+}
+
+variable "iso_datastore" {
+  type = string
+}
