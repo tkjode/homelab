f91c0c07b1f502e9e8e67d6c3f330b6968de0b2e
TKJode <tkjode@gmail.com>
2025-11-18 20:59:11 -0500

    bugfix(regulus): attempt to fix indentation of nameserver addresses in network template file

--- base/builds/regulus/bridge-regulus.tf
+++ base/builds/regulus/bridge-regulus.tf
@@ -1,5 +1,7 @@
 resource "proxmox_virtual_environment_network_linux_bridge" "vmbr10" {
   node_name   = var.proxmox_node
   name        = "vmbr10"
+  autostart   = true
+  vlan_aware  = false
   comment     = "Regulus Bridge"
 }
