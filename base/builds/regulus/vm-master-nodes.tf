3b9b1a948768f8b4313a295d94598757fee4c154
TKJode <tkjode@gmail.com>
2025-11-22 20:45:13 -0500

    feat(regulus): adding http health checks to tcp passthrough fun

--- base/builds/regulus/vm-master-nodes.tf
+++ base/builds/regulus/vm-master-nodes.tf
@@ -69,9 +69,10 @@ resource "proxmox_virtual_environment_file" "master-user-data-cloud-config" {
     data        = templatefile(
                     "cloud-init/k8s-master/master-user-data.yaml.tftpl",
                     {
-                      hostname   = join("-", ["master", count.index] ),
-                      control-plane-endpoint = cidrhost(join("/", [var.gw_net_cluster.network, var.gw_net_cluster.mask]), var.gw_net_cluster.cidr),
-                      cluster-join-token = var.cluster-join-token
+                      hostname                = join("-", ["master", count.index] ),
+                      control-plane-endpoint  = cidrhost(join("/", [var.gw_net_cluster.network, var.gw_net_cluster.mask]), var.gw_net_cluster.cidr),
+                      cluster-join-token      = var.cluster-join-token,
+                      node_select             = count.index
                     }
                   )
   }
