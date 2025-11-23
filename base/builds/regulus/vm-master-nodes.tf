2c655e2a21fb85e4d986f5794febfc0861a88494
TKJode <tkjode@gmail.com>
2025-11-22 20:47:03 -0500

    bugfix(regulus): fix variable name underscore to dash as expected

--- base/builds/regulus/vm-master-nodes.tf
+++ base/builds/regulus/vm-master-nodes.tf
@@ -72,7 +72,7 @@ resource "proxmox_virtual_environment_file" "master-user-data-cloud-config" {
                       hostname                = join("-", ["master", count.index] ),
                       control-plane-endpoint  = cidrhost(join("/", [var.gw_net_cluster.network, var.gw_net_cluster.mask]), var.gw_net_cluster.cidr),
                       cluster-join-token      = var.cluster-join-token,
-                      node_select             = count.index
+                      node-select             = count.index
                     }
                   )
   }
