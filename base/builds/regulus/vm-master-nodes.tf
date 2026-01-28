8ae5df727d4c634ae0d13a05102e573bf27c9de5
TKJode <tkjode@gmail.com>
2026-01-27 21:31:35 -0500

    fix(regulus): fix cyclical depends

--- base/builds/regulus/vm-master-nodes.tf
+++ base/builds/regulus/vm-master-nodes.tf
@@ -6,7 +6,7 @@ resource "proxmox_virtual_environment_vm" "masters" {
   node_name                 = var.proxmox_node
   stop_on_destroy           = true
   vm_id                     = var.proxmox-vmid-offset + var.master-ip-offset + count.index
-  depends_on                = [ proxmox_virtual_environment_vm.regulus-gateway, null_resource.wait-for-haproxy-response ]
+  depends_on                = [ null_resource.wait-for-haproxy-response ]
   pool_id                   = proxmox_virtual_environment_pool.cluster.pool_id
 
   agent {
