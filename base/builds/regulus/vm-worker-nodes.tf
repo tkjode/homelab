64f52ad57158a9bd1db31b87047e73a3cfc8cda8
TKJode <tkjode@gmail.com>
2026-01-27 21:29:11 -0500

    fix(regulus): fix cyclical depends

--- base/builds/regulus/vm-worker-nodes.tf
+++ base/builds/regulus/vm-worker-nodes.tf
@@ -6,7 +6,7 @@ resource "proxmox_virtual_environment_vm" "workers" {
   node_name                 = var.proxmox_node
   stop_on_destroy           = true
   vm_id                     = var.proxmox-vmid-offset + var.worker-ip-offset + count.index
-  depends_on                = [ proxmox_virtual_environment_vm.regulus-gateway, proxmox_virtual_environment_vm.masters, proxmox_virtual_environment_vm.bastion ]
+  depends_on                = [ proxmox_virtual_environment_vm.regulus-gateway, null_resource.wait-for-kubernetes-api ]
   pool_id                   = proxmox_virtual_environment_pool.cluster.pool_id
 
   lifecycle {
