205fd426a7944a95758cef82ccddc5fe21ab077d
TKJode <tkjode@gmail.com>
2026-02-16 15:30:58 -0500

    fix(nas): device will not boot, switching to import type disk image for qcow2 direct hdd boot

--- base/builds/nas/iso-debian-trixie-daily.tf
+++ base/builds/nas/iso-debian-trixie-daily.tf
@@ -1,8 +1,8 @@
 resource "proxmox_virtual_environment_download_file" "debian-trixie" {
-  content_type        = "iso"
+  content_type        = "import"
   datastore_id        = var.iso_datastore
   overwrite_unmanaged = true
-  file_name           = "debian-trixie.img"
+  file_name           = "debian-trixie.qcow2"
   node_name           = var.proxmox_node
   url                 = "https://cloud.debian.org/images/cloud/trixie/latest/debian-13-generic-arm64.qcow2"
 }
