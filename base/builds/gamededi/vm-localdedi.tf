96a9990fa201427860ae75743a82f8a49ad27dad
TKJode <tkjode@gmail.com>
2026-05-02 15:10:55 -0400

    chore(gamededi): rebuild both servers from scratch on ubuntu resolute 26.04 and fix docker durability

--- base/builds/gamededi/vm-localdedi.tf
+++ base/builds/gamededi/vm-localdedi.tf
@@ -43,7 +43,7 @@ resource "proxmox_virtual_environment_vm" "localdedi" {
   # Boot Disk
   disk {
     datastore_id            = "SSD"
-    file_id                 = proxmox_virtual_environment_download_file.ubuntu-cloud-server-iso.id
+    file_id                 = proxmox_download_file.ubuntu-cloud-server-iso.id
     size                    = 25
     interface               = "scsi0"
   }
