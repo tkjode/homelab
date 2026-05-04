e93e88e427e775dacd3abb5c21bfbc8fc8722f3f
TKJode <tkjode@gmail.com>
2026-05-04 18:08:05 -0400

    chore(regulus): Boot up Ubuntu 26.04 Resolute as base image

--- base/builds/regulus/iso-ubuntu-cloud.tf
+++ base/builds/regulus/iso-ubuntu-cloud.tf
@@ -1,9 +1,19 @@
-resource "proxmox_virtual_environment_download_file" "ubuntu-cloud-server-iso" {
+# resource "proxmox_virtual_environment_download_file" "ubuntu-cloud-server-iso" {
+#   content_type        = "iso"
+#   datastore_id        = var.iso_datastore
+#   overwrite_unmanaged = true
+#   overwrite           = false   # Do not push when iso chgs
+#   file_name           = "ubuntu-25.10-server-cloudimg-amd64.img"
+#   node_name           = var.proxmox_node
+#   url                 = "https://cloud-images.ubuntu.com/releases/questing/release/ubuntu-25.10-server-cloudimg-amd64.img"
+# }
+
+resource "proxmox_download_file" "ubuntu-cloud-server-iso" {
   content_type        = "iso"
   datastore_id        = var.iso_datastore
   overwrite_unmanaged = true
-  overwrite           = false   # Do not push when iso chgs
-  file_name           = "ubuntu-25.10-server-cloudimg-amd64.img"
+  overwrite           = false
+  file_name           = "regulus-resolute-server-cloudimg-amd64.img"
   node_name           = var.proxmox_node
-  url                 = "https://cloud-images.ubuntu.com/releases/questing/release/ubuntu-25.10-server-cloudimg-amd64.img"
+  url                 = "https://cloud-images.ubuntu.com/resolute/20260421/resolute-server-cloudimg-amd64.img"
 }
