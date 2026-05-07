eed881d0e46b49803a235dee645ffa902a65a7b4
TKJode <tkjode@gmail.com>
2026-05-07 10:54:02 -0400

    fix(regulus:hack): fix slashes in cluster maintenance script

--- hack/reboot_regulus.sh
+++ hack/reboot_regulus.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-ssh bastion kubectl get nodes -o json | /
-  jq -rf genClusterMaintenanceCommands.jq | /
-  xargs -L 1 -I {} sh -c "{}"
+ssh bastion kubectl get nodes -o json  \
+  | jq -rf genClusterMaintenanceCommands.jq  \
+  | xargs -L 1 -I {} sh -c "{}"
