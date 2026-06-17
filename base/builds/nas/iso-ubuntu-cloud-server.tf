9aa04bc9c4a1c15a5c1f7daab87322a8191bfc75
TKJode <tkjode@gmail.com>
2026-06-17 11:42:53 -0400

    chore(nas): bump to stonking next build

--- base/builds/nas/iso-ubuntu-cloud-server.tf
+++ base/builds/nas/iso-ubuntu-cloud-server.tf
@@ -2,7 +2,7 @@ resource "proxmox_virtual_environment_download_file" "ubuntu-cloud-server-iso" {
   content_type        = "iso"
   datastore_id        = var.iso_datastore
   overwrite_unmanaged = true
-  file_name           = "nas-ubuntu-25.10-server-cloudimg-amd64.img"
+  file_name           = "nas-stonking-server-cloudimg-amd64.img"
   node_name           = var.proxmox_node
-  url                 = "https://cloud-images.ubuntu.com/releases/questing/release/ubuntu-25.10-server-cloudimg-amd64.img"
+  url                 = "https://cloud-images.ubuntu.com/stonking/20260612/stonking-server-cloudimg-amd64.img"
 }
