39e66645722b86ce3a03dde33bdf8987760a9c07
TKJode <tkjode@gmail.com>
2026-06-17 11:42:13 -0400

    chore(gamededi): bump to stonking on next rebuild

--- base/builds/gamededi/iso-ubuntu-cloud-server.tf
+++ base/builds/gamededi/iso-ubuntu-cloud-server.tf
@@ -2,7 +2,7 @@ resource "proxmox_download_file" "ubuntu-cloud-server-iso" {
   content_type        = "iso"
   datastore_id        = var.iso_datastore
   overwrite_unmanaged = true
-  file_name           = "gamededi-resolute-server-cloudimg-amd64.img"
+  file_name           = "gamededi-stonking-server-cloudimg-amd64.img"
   node_name           = var.proxmox_node
-  url                 = "https://cloud-images.ubuntu.com/resolute/20260421/resolute-server-cloudimg-amd64.img"
+  url                 = "https://cloud-images.ubuntu.com/stonking/20260612/stonking-server-cloudimg-amd64.img"
 }
