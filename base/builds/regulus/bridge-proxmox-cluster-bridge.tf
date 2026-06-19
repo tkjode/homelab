97a15c5fb19f88b59d41ac49bd2a8f511c144b77
TKJode <tkjode@gmail.com>
2026-06-19 11:38:27 -0400

    fix(regulus:ceph): adding /mask to CIDR address for bridge

--- base/builds/regulus/bridge-proxmox-cluster-bridge.tf
+++ base/builds/regulus/bridge-proxmox-cluster-bridge.tf
@@ -4,8 +4,11 @@ resource "proxmox_network_linux_bridge" "proxmox-cluster-bridge" {
   autostart   = true
   vlan_aware  = false
   comment     = "Regulus Bridge"
-  address     = cidrhost(
-                  join("/", [var.gw-net-cluster.network, var.gw-net-cluster.mask]), 
-                  var.proxmox-ceph-offset 
-                )
+  address     = join("/", [ 
+                  cidrhost(
+                    join("/", [var.gw-net-cluster.network, var.gw-net-cluster.mask]), 
+                    var.proxmox-ceph-offset 
+                  ),
+                  var.gw-net-cluster.mask
+                ])
 }
