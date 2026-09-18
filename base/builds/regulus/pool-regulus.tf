dfd6db8ac377e73dba602643095b6084481bbbd1
TKJode <tkjode@gmail.com>
2026-09-18 11:30:20 -0400

    fix(regulus): never delete pool

--- base/builds/regulus/pool-regulus.tf
+++ base/builds/regulus/pool-regulus.tf
@@ -1,4 +1,7 @@
 resource "proxmox_virtual_environment_pool" "cluster" {
   comment   = join(" ", [var.cluster-name, "cluster assets"])
   pool_id   = join(".", [var.cluster-name, var.cluster-domain])
+  lifecycle {
+    prevent_destroy = true
+  }
 }
